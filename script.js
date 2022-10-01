075c8a62f10c92f7a27f5bc5846e0b97c2978c0f
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-09-30 19:14:10 -0500

    added hour keys

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-//date picker
-var dayWeek= moment().format("dddd, MMMM Do ")
-$("#currentDay").text(dayWeek);
+//load html and css first
 
-console.log(dayWeek);
+  //display current day of week, month, and date
+  var dayWeek = moment().format("dddd, MMMM Do ");
+  $("#currentDay").text(dayWeek);
+
+//assign save button listening for the user input
+// $(".saveBtn").on("click", function (event) {
+
+
+// }
+//set localStorage 
+
+//
+
+
+
+//main function
+function timeTracker (){
+
+//get current time
+
+//loop over time blocks 
+for (var i=0; i < time-block.length; i++)
+{
+
+}};
+
+
+//rerun funtion
+timeTracker();
