50cb07a580d28989149f2711ab7a987bfd9d3082
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-03 18:26:58 -0500

    update UI

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 //load html and css first
-
+$(document).ready(function () {
   //display current day of week, month, and date
   var dayWeek = moment().format("dddd, MMMM Do ");
-  $("#currentDay").text(dayWeek);
+  $("#today").text(dayWeek);
 
-//assign save button listening for the user input
-// $(".saveBtn").on("click", function (event) {
+  $(".saveBtn").addEventListener("click", function () {
+    //assign save button listening for the user input
+    $(".saveBtn").on("click", function () {
+      var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+      var time = $(this).parent().attr("#currentDay"); // taken the change from the parent html id attribute
 
+      //set localStorage
 
-// }
-//set localStorage 
+      localStorage.setItem(time, text);
+    });
 
-//
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11.description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
 
+    // function timeTracker (){
+    //   var currentTime = moment().hour();
 
-
-//main function
-function timeTracker (){
-
-//get current time
-
-//loop over time blocks 
-for (var i=0; i < time-block.length; i++)
-{
-
-}};
-
-
-//rerun funtion
-timeTracker();
+    //   for(var i=0; i > time-block.length; i++)
+  });
+});
